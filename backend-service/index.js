8dbc65e3727f20fda0bbff7cf5e1e728f61df9fb
kainunka <joker.200mb@gmail.com>
2020-11-14 01:30:43 +0700

    firstcommit

---
+++ backend-service/index.js
@@ -0,0 +1,92 @@
+// GET /data 
+// GET /data/1
+// POST /data 
+// PUT /data/1 
+// DELETE /data/1 
+
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser");
+
+const Users = require('./model/users')
+
+const options = {
+    user: 'kainunka',
+    pass: 'yukimuraseichi'
+};
+const version = "v1"
+const port = process.env.PORT || 5000;
+
+mongoose.connect(`mongodb+srv://${options.user}:${options.pass}@cluster0.iwbh3.mongodb.net/sampeng?retryWrites=true&w=majority`)
+
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+
+app.get("/api/"+version+"/users", (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+
+    Users.find({}, (err, results) => {
+        if (err) return res.json({ status: false, message: "cannot get all users", code: err });
+
+        if (results) {
+            res.json({ status: true, message:"Successfully get product.", data: results })
+        }
+    })
+})
+
+app.get("/api/"+version+"/users/:id", (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    const { id } = req.params
+
+    Users.findById(id, (err, results) => {
+        if (err) return res.json({ status: false, message: "cannot get users by id", code: err })
+        if (results) {
+            res.json({ status: true, message:"Successfully get users.", data: results })
+        }
+    })
+})
+
+app.delete("/api/"+version+"/users/:id", (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    const { id } = req.params
+
+    Users.findByIdAndDelete(id, (err, results) => {
+        if (err) return res.json({ status: false, message: "cannot remove users", code: err })
+        if (results) {
+            res.json({ status: true, message: "Successfully remove users" })
+            res.status(204).end()
+        }
+    })
+})
+
+app.put("/api/"+version+"/users/:id", (req, res) => {
+    const payload = req.body
+    const { id } = req.params
+
+    Users.findByIdAndUpdate(id, { $set: payload }, (err, results) => {
+        if (err) {
+            res.json({ status: false, message: "cannot update users", code: err })
+        } else {
+            res.json({ status: true, message: "Successfully update users" })
+        }
+    })
+})
+
+app.post("/api/"+version+"/users", (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+
+    const users = new Users(req.body)
+    users.save(req.body, (err, doc) => {
+        if (err) return res.json({ status: false, message: "cannot add users", code: err });
+        res.json({ status: true, message:"Successfully add users."})
+    })
+})
+
+app.listen(port, () => {
+    console.log("application is listening on:", port);
+ })
+
+ module.exports = app;
